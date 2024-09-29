2030bed784bdf53db31ae7ee80d55165e6eab53d
DESK-VM\TellesVM <victor.telles@iteso.mx>
2024-09-28 20:21:19 -0600

    Implementacion de tabla de Producto modelo

---
+++ src/models/productoModel.js
@@ -0,0 +1,29 @@
+const  { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+//Definicion del modelo de Producto (tabla)
+const Producto = sequelize.define('Producto', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    unidad_medida: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    precio_base: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    }
+}, {
+    tableName: 'productos',
+    timestamp: false
+});
+
+module.exports = Producto;
