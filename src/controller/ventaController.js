8a4a08c01249405234e8858aa3ae7d6dc39c6517
DESK-VM\TellesVM <victor.telles@iteso.mx>
2024-09-29 12:04:50 -0600

    Implementacion de lal logica de notificaciones

--- src/controller/ventaController.js
+++ src/controller/ventaController.js
@@ -1,6 +1,9 @@
 const AWS = require('aws-sdk');
 const httpCodes = require('../types/http-codes');
 const { NotaVenta } = require('../models/notaVentaModel');
+const PDFDocument = require('pdfkit');
+const s3 = new AWS.S3();
+const sns = new AWS.SNS();
 
 // Configuración de AWS
 AWS.config.update({
@@ -12,28 +15,28 @@ class VentaController {
 
     //Obtener todas las ventas
     async getAllventas(req, res) {
-        try{
+        try {
             const ventas = await NotaVenta.findAll();
             res.status(httpCodes.OK).json(ventas);
 
         } catch (error) {
             console.error("Error al obtener las notas de venta: ", error);
-            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error al obtener las ventas'})
+            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error al obtener las ventas' })
         }
     }
 
     //Obtener venta por ID
-    async getVentaById(req, res){
-        try{
+    async getVentaById(req, res) {
+        try {
             const venta = await NotaVenta.findByPk(req.params.id);
             if (!venta) {
-                res.status(httpCodes.NOT_FOUND).json({ message: 'Nota de venta no encontrada'});
+                res.status(httpCodes.NOT_FOUND).json({ message: 'Nota de venta no encontrada' });
             }
             //Venta encontrada
             res.status(httpCodes.OK).json(venta);
-        } catch  (error) {
+        } catch (error) {
             console.error('Error al obtener nota de venta por ID:', error);
-            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error al obtener la nota de venta'});
+            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error al obtener la nota de venta' });
         }
     }
 
@@ -41,20 +44,79 @@ class VentaController {
     async createVenta(req, res) {
         try {
             const { clienteId, direccionFacturacion, direccionEnvio, totalNota } = req.body;
+            //Nueva venta
             const nuevaVenta = await NotaVenta.create({
                 clienteId,
                 direccionFacturacion,
                 direccionEnvio,
                 totalNota
-            })
+            });
 
-            //Implementar en un futuro la generacion de PDF y subirse a S3
+            //Generacion de PDF
+            const doc = new PDFDocument();
+            const pdfBuffers = [];
 
-            //Se creo nueva venta
-            res.status(httpCodes.CREATED).json(nuevaVenta);
+            doc.on('data', (chunk) => pdfBuffers.push(chunk));
+
+            //Promesa del pdf
+            const pdfDataPromise = new Promise((resolve, reject) => {
+                doc.on('end', () => {
+                    const pdfData = Buffer.concat(pdfBuffers);
+                    resolve(pdfData);
+                });
+                doc.on('error', (err) => reject(err));
+            });
+
+            //Info PDF
+            doc.text(`Nota de venta #${nuevaVenta.id}`);
+            doc.text(`Cliente ID:: ${clienteId}`);
+            doc.text(`Dirección de facturación: ${direccionFacturacion}`);
+            doc.text(`Dirección de envío: ${direccionEnvio}`);
+            doc.text(`Total: $${totalNota}`);
+            doc.end();
+
+            //Esperar a la generacion del pdf
+            const pdfData = await pdfDataPromise;
+
+            //Subir PDf a S3
+            const pdfKey = `notas-de-venta/nota_${nuevaVenta.id}.pdf`;
+            const params = {
+                Bucket: process.env.S3.BUCKET_NAME,
+                Key: pdfKey,
+                Body: pdfData,
+                ContentType: 'application/pdf',
+                ACL: 'public-read', //Enlace de descarga
+            };
+
+            await s3.upload(params).promise();
+
+            //URL de pdf en s3
+            const pdfUrl = `https://${process.env.S3_BUCKET_NAME}.s3.amazonaws.com/${pdfKey}`;
+
+            const snsParams = {
+                Message: `Gracias por su compra. Aquí tiene la nota de venta #${nuevaVenta.id}. Puede descargar el PDF de la nota en el siguiente enlace: ${pdfUrl}`,
+                Subject: `Nota de venta #${nuevaVenta.id}`,
+                TopicArn: process.env.SNS.TOPIC_ARN,
+                MessageAttributes: {
+                    'AWS.SNS.SMS.SMSType': {
+                        DataType: 'String',
+                        StringValue: 'Transactional',
+                    }
+                }
+            };
+
+            //Enviar notificacion
+            await sns.publish(snsParams).promise();
+
+            //Notf: Creada la venta
+            res.status(httpCodes.CREATED).json({
+                venta: nuevaVenta,
+                pdfUrl
+            });
+
         } catch (error) {
             console.error('Error al crear nueva nota de venta:', error);
-            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ error: 'Error al crear la nota de venta'});
+            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ error: 'Error al crear la nota de venta' });
         }
     }
 
@@ -65,17 +127,57 @@ class VentaController {
             const { direccionFacturacion, direccionEnvio, totalNota } = req.body;
 
             const venta = await NotaVenta.findByPk(id);
-            if (!venta) {{}
-                return res.status(httpCodes.NOT_FOUND).json({ error: 'Nota de venta no encontrada'});
+            if (!venta) {
+                return res.status(httpCodes.NOT_FOUND).json({ error: 'Nota de venta no encontrada' });
             }
 
+            //Actualizar la venta en RDS
             await venta.update({
                 direccionFacturacion,
                 direccionEnvio,
                 totalNota
             });
+
+            //Generacion de un nuevo PDF
+            const doc = new PDFDocument();
+            const pdfBuffers = [];
+
+            doc.on('data', (chunk) => pdfBuffers.push(chunk));
+
+            const pdfDataPromise = new Promise((resolve, reject) => {
+                doc.on('end', () => {
+                    const pdfData = Buffer.concat(pdfBuffers);
+                    resolve(pdfData);
+                });
+                doc.on('error', (err) => reject(err));
+            });
+
+            //Info a actualizar el PDF
+            doc.text(`Nota de venta actualizada #${venta.id}`);
+            doc.text(`Cliente ID:: ${venta.clienteId}`);
+            doc.text(`Dirección de facturación: ${direccionFacturacion}`);
+            doc.text(`Dirección de envío: ${direccionEnvio}`);
+            doc.text(`Total actualizado: $${totalNota}`);
+            doc.end();
+
+            //Esperar la generacion del PDF
+            const pdfData =await pdfDataPromise;
+
+            //Subir a s3
+            const pdfKey = `notas-de-venta/nota_${venta.id}.pdf`;
+            const params = {
+                Bucket: process.env.S3_BUCKET_NAME,
+                Key: pdfKey,
+                Body: pdfData,
+                ContentType: 'application/pdf',
+                ACL: 'public-read',
+            };
+
+            await s3.upload(params).promise();
+
             //Nota venta Actulizada
-            res.status(httpCodes.OK).json(venta);
+            res.status(httpCodes.OK).json({ message: 'Venta y PDF actualizados', venta });
+
         } catch (error) {
             console.error('Error al actualizar nota de venta:', error);
             res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ error: 'Error al actualizar la nota de venta' });
@@ -83,16 +185,28 @@ class VentaController {
     }
 
     //Eliminar ventas
-    async deleteVenta(req, res){
+    async deleteVenta(req, res) {
         try {
             const { id } = req.params;
-            const venta= await NotaVenta.findByPk(id);
+            const venta = await NotaVenta.findByPk(id);
             if (!venta) {
-                return res.status(httpCodes.NOT_FOUND).json({ error: 'Nota de venta no encontrada'});
+                return res.status(httpCodes.NOT_FOUND).json({ error: 'Nota de venta no encontrada' });
             }
 
+            //Eliminar el pdf de s3
+            const pdfKey = `notas-de-venta/nota_${venta.id}.pdf`;
+            const params ={
+                Bucket: process.env.S3_BUCKET_NAME,
+                Key: pdfKey,
+            };
+
+            //Eliminar objeto en s3
+            await s3.deleteObject(params).promise();
+
+            //Eliminar venta en RDS
             await venta.destroy();
-            res.status(httpCodes.OK).json({ message: "Nota de venta eliminada"});
+            res.status(httpCodes.OK).json({ message: 'Nota de venta eliminada y PDF eliminado' });
+
         } catch (error) {
             console.error('Error al eliminar nota de venta:', error);
             res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ error: 'Error al eliminar la nota de venta ' })
@@ -100,8 +214,24 @@ class VentaController {
     }
 
     //Descargar PDF de una venta (falta)
-    downloadPDF(req, res) {
-        res.send('Descargando PDF de la venta con ID: '+ req.params.id);
+    async downloadPDF(req, res) {
+        try {
+            const { id } = req.params;
+            const venta = await NotaVenta.findByPk(id);
+            if (!venta) {
+                return res.status(httpCodes.NOT_FOUND).json({ message: 'Nota de venta no encontrada' });
+            }
+
+            //Generar la URL del PDF almacenadado en S3
+            const pdfKey = `notas-de-venta/nota_${venta.id}.pdf`;
+            const pdfUrl = `https://${process.env.S3_BUCKET_NAME}.s3.amazonaws.com/${pdfKey}`;
+
+            res.status(httpCodes.OK).json({ pdfUrl });
+
+        } catch (error) {
+            console.err('Error al descargar el PDF:' , error);
+            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ error: 'Error al descargar el PDF' });
+        }
     }
 }
 
