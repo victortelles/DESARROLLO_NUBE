debfd1c72af475c7b3d4a5b0a427efb28491cb01
DESK-VM\TellesVM <victor.telles@iteso.mx>
2024-09-28 22:50:11 -0600

    Implementacion de nota de venta

--- src/controller/ventaController.js
+++ src/controller/ventaController.js
@@ -1,29 +1,102 @@
 const AWS = require('aws-sdk');
+const httpCodes = require('../types/http-codes');
+const { NotaVenta } = require('../models/notaVentaModel');
 
 // Configuración de AWS
 AWS.config.update({
     region: process.env.AWS_REGION || 'us-east-1',
 });
 
-const s3 = new AWS.S3();
-const sns = new AWS.SNS();
-
 //Clase
 class VentaController {
 
     //Obtener todas las ventas
-    getAllventas(req, res) {
-        res.send('Obteniendo todas las ventas');
+    async getAllventas(req, res) {
+        try{
+            const ventas = await NotaVenta.findAll();
+            res.status(httpCodes.OK).json(ventas);
+
+        } catch (error) {
+            console.error("Error al obtener las notas de venta: ", error);
+            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error al obtener las ventas'})
+        }
     }
 
     //Obtener venta por ID
-    getVentaById(req, res){
-        res.send('Obteniendo venta por ID' + req.params.id);
+    async getVentaById(req, res){
+        try{
+            const venta = await NotaVenta.findByPk(req.params.id);
+            if (!venta) {
+                res.status(httpCodes.NOT_FOUND).json({ message: 'Nota de venta no encontrada'});
+            }
+            //Venta encontrada
+            res.status(httpCodes.OK).json(venta);
+        } catch  (error) {
+            console.error('Error al obtener nota de venta por ID:', error);
+            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error al obtener la nota de venta'});
+        }
     }
 
     //Crear nueva venta
-    createVenta(req, res) {
-        res.send('Venta Creada');
+    async createVenta(req, res) {
+        try {
+            const { clienteId, direccionFacturacion, direccionEnvio, totalNota } = req.body;
+            const nuevaVenta = await NotaVenta.create({
+                clienteId,
+                direccionFacturacion,
+                direccionEnvio,
+                totalNota
+            })
+
+            //Implementar en un futuro la generacion de PDF y subirse a S3
+
+            //Se creo nueva venta
+            res.status(httpCodes.CREATED).json(nuevaVenta);
+        } catch (error) {
+            console.error('Error al crear nueva nota de venta:', error);
+            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ error: 'Error al crear la nota de venta'});
+        }
+    }
+
+    //Actualizar ventas
+    async updateVenta(req, res) {
+        try {
+            const { id } = req.params;
+            const { direccionFacturacion, direccionEnvio, totalNota } = req.body;
+
+            const venta = await NotaVenta.findByPk(id);
+            if (!venta) {{}
+                return res.status(httpCodes.NOT_FOUND).json({ error: 'Nota de venta no encontrada'});
+            }
+
+            await venta.update({
+                direccionFacturacion,
+                direccionEnvio,
+                totalNota
+            });
+            //Nota venta Actulizada
+            res.status(httpCodes.OK).json(venta);
+        } catch (error) {
+            console.error('Error al actualizar nota de venta:', error);
+            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ error: 'Error al actualizar la nota de venta' });
+        }
+    }
+
+    //Eliminar ventas
+    async deleteVenta(req, res){
+        try {
+            const { id } = req.params;
+            const venta= await NotaVenta.findByPk(id);
+            if (!venta) {
+                return res.status(httpCodes.NOT_FOUND).json({ error: 'Nota de venta no encontrada'});
+            }
+
+            await venta.destroy();
+            res.status(httpCodes.OK).json({ message: "Nota de venta eliminada"});
+        } catch (error) {
+            console.error('Error al eliminar nota de venta:', error);
+            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ error: 'Error al eliminar la nota de venta ' })
+        }
     }
 
     //Descargar PDF de una venta
--- src/routes/ventas.js
+++ src/routes/ventas.js
@@ -15,6 +15,12 @@ router.get('/:id', (req, res) => ventaController.getVentaById(req, res));
 //crear una nueva venta
 router.post('/', (req, res) => ventaController.createVenta(req, res));
 
+//crear una nueva venta
+router.put('/:id', (req, res) => ventaController.updateVenta(req, res));
+
+//crear una nueva venta
+router.delete('/:id', (req, res) => ventaController.deleteVenta(req, res));
+
 //Descargar PDF de la venta
 router.get('/descargar/:id', (req, res) => ventaController.downloadPDF(req, res));
 
