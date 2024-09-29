aef776056e33b8b59f947c5c1342b0157bdb273f
DESK-VM\TellesVM <victor.telles@iteso.mx>
2024-09-28 22:20:14 -0600

    Implementacion del control de logica de productos.

--- src/controller/productoController.js
+++ src/controller/productoController.js
@@ -1,4 +1,6 @@
 const AWS = require('aws-sdk');
+const httpCodes = require('../types/http-codes');
+const db = require('../config/db');
 
 //Configuracion AWS.
 AWS.config.update({
@@ -11,28 +13,81 @@ const sns = new AWS.SNS();
 class ProductoController{
 
     //Obtener todo los productos
-    getAllProductos(req, res) {
-        res.send('Obteniendo los productos');
+    async getAllProductos(req, res) {
+        try {
+            const [productos] = await db.query('SELECT * FROM productos');
+            //obtenido
+            res.status(httpCodes.OK).json(productos);
+
+        } catch (error) {
+            console.error(error);
+            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error al obtener los productos' });
+        }
     }
 
     //Obtener todo los productos
-    getProductoById(req, res) {
-        res.send('Obteniendo producto con ID: ' + req.params.id);
+    async getProductoById(req, res) {
+        const { id } = req.params;
+        try {
+            const [producto] = await db.query('SELECT * FROM productos WHERE id = ?', [id]);
+            if (producto.lenght === 0) {
+                return res.status(httpCodes.NOT_FOUND).send('Producto no encontrado');
+            }
+            //Obtenido
+            res.status(httpCodes.OK).json(producto[0]);
+        } catch (error) {
+            console.error(error);
+            res.status(httpCodes.INTERNAL_SERVER_ERROR).send('Error al obtener el producto');
+        }
     }
 
     //Crear un nuevo producto
-    createProducto(req, res) {
-        res.send('Producto creado');
+    async createProducto(req, res) {
+        const {nombre, unidadMedida, precioBase } = req.body;
+        try {
+            const [result] = await db.query('INSERT INTO Productos (nombre, unidad_medida, precio_base) VALUES (?, ?, ?)', [nombre, unidadMedida, precioBase]);
+            //Creado
+            res.status(httpCodes.CREATED).json({ id: result.insertId, nombre, unidadMedida, precioBase });
+        } catch (error) {
+            console.error(error);
+            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error al crear el producto'});
+        }
     }
 
     //actualizar producto
-    updateProducto(req, res) {
-        res.send('Producto actualizado'  + req.params.id);
+    async updateProducto(req, res) {
+        const { id } = req.params;
+        const { nombre, unidadMedida, precioBase } = req.body;
+        try {
+            const [result] = await db.query('UPDATE Productos SET nombre = ?, unidad_medida = ?, precio_base = ? WHERE id = ?', [nombre, unidadMedida, precioBase, id]);
+            if (result.affectedRows === 0) {
+                return res.status(httpCodes.NOT_FOUND).send('Producto no encontrado');
+            }
+
+            //Actualizado
+            res.status(httpCodes.OK).send('Producto actualizado');
+
+        } catch (error) {
+            console.error(error);
+            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error al actualizar el producto'});
+
+        }
     }
 
     //Eliminar producto
-    deleteProducto(req, res){
-        res.send('Producto eliminado.' + req.params.id);
+    async deleteProducto(req, res){
+        const { id } = req.params;
+        try {
+            const [result] = await db.query('DELETE FROM Productos WHERE id = ?', [id]);
+            if (result.affectedRows === 0) {
+                return res.status(httpCodes.NOT_FOUND).send('Producto no encontrado');
+            }
+            //Eliminado
+            res.status(httpCodes.OK).send('Producto eliminado');
+        } catch (error) {
+            console.error(error);
+            res.status(httpCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error al eliminar el producto'});
+        }
     }
 }
 
